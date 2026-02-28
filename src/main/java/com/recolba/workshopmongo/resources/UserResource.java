ebedc18e35c8764ba0b9155ddf1c31cf5b698da7
Renato Barbosa <renatocolbar@gmail.com>
2026-02-28 13:33:08 -0300

    Atualização de usuário com PUT - Correção

--- src/main/java/com/recolba/workshopmongo/resources/UserResource.java
+++ src/main/java/com/recolba/workshopmongo/resources/UserResource.java
@@ -60,7 +60,6 @@ public class UserResource {
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
 		User obj = service.fromDTO(objDTO);
 		obj.setId(id);
-		obj = service.insert(obj);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
