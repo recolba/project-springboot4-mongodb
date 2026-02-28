9d8970401a1fb83ee4d2b14d28118a32561a69d3
Renato Barbosa <renatocolbar@gmail.com>
2026-02-28 20:44:17 -0300

    Consulta com vários critérios

--- src/main/java/com/recolba/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/recolba/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.recolba.workshopmongo.repository;
 
+import java.util.Date;
+
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,11 +11,14 @@ import org.springframework.stereotype.Repository;
 import com.recolba.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
-	
+public interface PostRepository extends MongoRepository<Post, String> {
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
 }
